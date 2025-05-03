49405034d366d3f7d553e3b02a2f7d8e66d894a9
Gabriel Silveira <jgabrielbsilveirag@gmail.com>
2025-05-03 19:25:29 -0300

    meu primeiro commit

---
+++ tests/support/database.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg')
+
+const DbConfig = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'zombieplus',
+    password: 'pwd123',
+    port: 5432
+
+}
+
+export async function executeSQL(sqlScript) {
+
+    try {
+        const pool = new Pool(DbConfig)
+        const client = await pool.connect()
+
+        const result = await client.query(sqlScript)
+        consonle.log(result.rows)
+    } catch (error) {
+        console.log('Erro ao executar SQL ' + error)
+
+    }
+
+
+}
